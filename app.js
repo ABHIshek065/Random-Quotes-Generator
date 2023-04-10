543d4b9e541570b05aa66567ac25d65027446f5c
Abhishek Patel <98528020+Abhi8755@users.noreply.github.com>
2023-04-10 18:33:36 +0530

    loader

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 let para = document.getElementById("para");
+let content = document.getElementById('content')
+let loader = document.getElementById('loader')
+let btn = document.getElementsByClassName('.btn')
+
+
+let loadingStatus = false;
+
+function checkLoaderStatus() {
+  content.style.display = "none";
+  if (loadingStatus == true) {
+    loader.classList.add("loader");
+  } else {
+    loader.classList.remove("loader");
+  }
+}
+
+
+
 
 function randomQuotes() {
+loadingStatus =true
+  checkLoaderStatus()
+
   fetch("https://type.fit/api/quotes")
     .then(function (response) {
       return response.json();
@@ -10,5 +31,8 @@ function randomQuotes() {
       document.getElementById("heading").innerText = data[random].text;
       document.getElementById("author").innerText = data[random].author;
       para.classList.remove('none')
+      loadingStatus = false
+      checkLoaderStatus()
+      content.style.display = "block";
     });
 }
